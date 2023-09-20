061b9225b0b05d1fbac851a874e99d062f2d6f77
JosephKagwi <josephkagwi28@gmail.com>
2023-09-20 23:48:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newCustomer) {
+    window.bestCustomer = newCustomer;
+  }
+
+
+
+  function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'Alice';
+    leastFavoriteCustomer = 'Bob'; 
+  }
